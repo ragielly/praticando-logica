48bb004319c612328113ababbbc2a8e33801fb3a
ragielly <ragielly.19@gmail.com>
2025-10-08 09:09:39 -0300

    iniciando funcao sortearNumero

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortearNumero(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    console.log(quantidade);
+    console.log(de);
+    console.log(ate);
+}
