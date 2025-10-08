4fefb906897976ecf47e44a7f560fdc6dad8fae9
ragielly <ragielly.19@gmail.com>
2025-10-08 17:09:20 -0300

    adicionando funcionalidades e finalizando projeto alugames

---
+++ alugames/js/app.js
@@ -0,0 +1,33 @@
+let jogosAlugados = 0;
+
+function ContarJogosAlugados(){
+    console.log(`Total de jogos alugados é  ${jogosAlugados}`);
+}
+
+function alterarStatus(id){ 
+    
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao =gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+        if(confirm(`Você tem certeza que quer devolver o jogo ${nomeJogo.textContent}?`)){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+    }else{
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
+    }
+    ContarJogosAlugados();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    ContarJogosAlugados();
+});
